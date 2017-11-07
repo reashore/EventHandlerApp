3818aab11e0d0deeb2f98d13121a86f933e5b0eb
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-06 17:18:04 -0800

    Working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CounterParent from './CounterParent';
+import CounterParent from './Components/CounterParent';
 import registerServiceWorker from './registerServiceWorker';
 
 var destination = document.getElementById('root');
