f4662ce0e73d1335d88c95fc8b837ee7d1de4652
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-06 16:52:29 -0800

    saving

--- src/index.js
+++ src/index.js
@@ -1,68 +1,9 @@
 
-import React, { Component }  from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import CounterParent from './CounterParent';
 import registerServiceWorker from './registerServiceWorker';
-
-class Counter extends Component {
-    render() {
-      var counterStyle = {
-        fontSize: 72,
-        fontFamily: "sans-serif",
-        color: "#333",
-        fontWeight: "bold"
-      };
-  
-      return (
-        <div style={counterStyle}>
-          {this.props.display}
-        </div>
-      );
-    }
-  }
-  
-  class CounterParent extends Component {
-    constructor() {
-      super();
-  
-      this.state = {
-        count: 0
-      }
-    }
-  
-    render() {
-      var backgroundStyle = {
-        padding: 50,
-        backgroundColor: "#FFC53A",
-        width: 250,
-        height: 100,
-        borderRadius: 10,
-        textAlign: "center"
-      };
-  
-      var buttonStyle = {
-        fontSize: "1em",
-        width: 60,
-        height: 30,
-        fontFamily: "sans-serif",
-        color: "#333",
-        fontWeight: "bold",
-        lineHeight: "3px"
-      };
-  
-      return (
-        <div>
-          <h1>Event Handler App</h1>
-          <div style={backgroundStyle}>
-            <Counter display={this.state.count} />
-            <button style={buttonStyle}>+</button>
-          </div>
-        </div>
-      );
-    }
-  }
-  
 
 var destination = document.getElementById('root');
 ReactDOM.render(<CounterParent />, destination);
